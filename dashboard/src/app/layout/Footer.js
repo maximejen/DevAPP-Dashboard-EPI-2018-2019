1340483f0a755723912d2bcd1c5c61635eec1d5b
Maxime JENNY <maxime.jenny@epitech.eu>
2018-10-10 23:14:12 +0200

    new css and some edit on the footer.

--- dashboard/src/app/layout/Footer.js
+++ dashboard/src/app/layout/Footer.js
@@ -3,12 +3,29 @@ import React from 'react'
 const Footer = () => (
     <footer className="footer">
         <div className="container">
-            <div className="content has-text-centered">
-                <p>
+            <div className="content is-centered columns is-0">
+                <div className={"column is-4"}/>
+                <img className={"column is-1"}
+                     style={{
+                         borderTopLeftRadius: '50%',
+                         borderTopRightRadius: '50%',
+                         borderBottomLeftRadius: '50%',
+                         borderBottomRightRadius: '50%',
+                         marginRight: 15,
+                         width: "90px",
+                         height: "90px"
+                     }}
+                     src="/logo.svg"
+                     alt="logo"
+                />
+                <div className={"column is-3"} style={{
+                    paddingTop: "1.5rem"
+                }}>
                     Built on <strong>React</strong> with <strong>Bulma</strong>
-                    <br />
+                    <br/>
                     Copyright 2018
-                </p>
+                </div>
+                <div className={"column is-4"}/>
             </div>
         </div>
     </footer>
