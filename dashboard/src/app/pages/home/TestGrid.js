8d978afe6d15872934b678a7137f03c8ba790531
Maxime JENNY <maxime.jenny@epitech.eu>
2018-10-10 22:01:43 +0200

    Push the fix that let you take the fils in other folders that src and also added the react-grid layout dependency that give you a grid of elements resizable and drag & dropable.

--- dashboard/src/app/pages/home/TestGrid.js
+++ dashboard/src/app/pages/home/TestGrid.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import "/node_modules/react-grid-layout/css/styles.css";
-import "/node_modules/react-resizable/css/styles.css";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 
 import ResponsiveGridLayout from 'react-grid-layout';
 
@@ -9,15 +9,15 @@ class TestGrid extends React.Component {
     render() {
         // layout is an array of objects, see the demo for more complete usage
         let layout = [
-            {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-            {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-            {i: 'c', x: 4, y: 0, w: 1, h: 2}
+            {i: '0', x: 0, y: 0, w: 1, h: 2, static: true},
+            {i: '1', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+            {i: '2', x: 4, y: 0, w: 1, h: 2}
         ];
         return (
             <ResponsiveGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-                <div key="a">a</div>
-                <div key="b">b</div>
-                <div key="c">c</div>
+                <div key="0">Chibre</div>
+                <div key="1">b</div>
+                <div key="2">c</div>
             </ResponsiveGridLayout>
         )
     }
