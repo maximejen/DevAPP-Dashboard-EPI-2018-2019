ab3523c7bab072d074a3db43d3fdbaa5bfc9e4a0
Maxime JENNY <maxime.jenny@epitech.eu>
2018-10-12 14:19:03 +0200

    Push the login page

---
+++ dashboard/src/app/pages/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div style={{
+                backgroundImage: 'linear-gradient(to right, #00F18E , #00A0FD)',
+                minHeight: "100%",
+                backgroundSize: "cover",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "80vh"
+            }}>
+                Login Page
+            </div>
+        );
+    }
+}
+
+export default Login
